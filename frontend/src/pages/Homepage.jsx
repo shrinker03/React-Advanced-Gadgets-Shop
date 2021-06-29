e110274f4f160927cc14b509126cf54c4dcf2630
shivam damre <shivamdamre913@gmail.com>
2021-06-29 11:21:18 +0530

    created: Backend express server created and product list uploaded on the server

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
+import axios from 'axios';
 import Product from '../components/Product';
 
 const Homepage = () => {
+const [products, setProducts] = useState([])
+
+useEffect(() => {
+    const fetchProducts = async () => {
+        const {data} = await axios.get('/products')
+        setProducts(data)
+    }
+
+    fetchProducts()
+}, [])
+ 
     return (
         <div>
             <h1>Latest Products</h1>
--- frontend/src/pages/Productpage.jsx
+++ frontend/src/pages/Productpage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
-const Productpage = ({match}) => {
-    const product = products.find((p)=> p._id === match.params.id)
+const Productpage = ({ match }) => {
+const [product, setProduct] = useState({})
+
+useEffect(() => {
+    const fetchProduct = async () => {
+        const {data} = await axios.get(`/products/${match.params.id}`)
+        setProduct(data)
+    }
+    fetchProduct()
+}, [match])
+
     return (
         <div>
             <Link className="btn btn-light my-3" to="/">
