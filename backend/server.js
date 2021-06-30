64408f71c9f13127baba09bb37e3c9f3db32f7f2
shivam damre <shivamdamre913@gmail.com>
2021-06-30 11:09:20 +0530

    created: mongoose config file to create a project to batabase created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors  from 'colors'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -21,4 +25,4 @@ app.get("/products/:id", (req, res) => {
 
 const  PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.underline.bold))
