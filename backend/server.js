b102d32697b51f75e789db0b1b23a3c004b5ae35
shivam damre <shivamdamre913@gmail.com>
2021-07-01 10:52:31 +0530

    created: route created to fetch the product data from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors  from 'colors'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +14,7 @@ app.get("/", (req, res) => {
     res.send("API is running")
 })
 
-app.get("/products", (req, res) => {
-    res.json(products)
-})
-
-app.get("/products/:id", (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/products', productRoutes)
 
 const  PORT = process.env.PORT || 5000
 
