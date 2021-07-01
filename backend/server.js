3981c5670060c7276a552e0f11e5a7a7bbdafc7e
shivam damre <shivamdamre913@gmail.com>
2021-07-01 11:06:00 +0530

    created: error handler middleware created to handle the errors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors  from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import {errorHandler, notFound} from './middleware/errorMiddeware.js'
 
 dotenv.config()
 
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
 })
 
 app.use('/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const  PORT = process.env.PORT || 5000
 
