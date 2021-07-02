ea170419ca03d2af52b3709db6c7f26ab7eb1cb1
shivam damre <shivamdamre913@gmail.com>
2021-07-02 10:53:34 +0530

    added: reducer, react-reducer, redux-thunk, redux-devtools created: productlist- reducer,action and constants created successfully

---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productListReducers';
+
+const reducer = combineReducers({
+    productList: productListReducer
+})
+
+const initialState = {}
+
+const middleWare = [thunk]
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleWare))
+)
+
+export default store
