7cf30c3a49286560a3103bbaebe4abf947ea34aa
shivam damre <shivamdamre913@gmail.com>
2021-07-03 13:12:58 +0530

    added: dispatch hook added to the homepage component to connect the database and backend to the frontend

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,30 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import {listProducts} from '../actions/productListActions'
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const Homepage = () => {
-const [products, setProducts] = useState([])
+const dispatch = useDispatch()
+
+const productList = useSelector(state => state.productList)
+const {loading, error, products} = productList
 
 useEffect(() => {
-    const fetchProducts = async () => {
-        const {data} = await axios.get('/products')
-        setProducts(data)
-    }
-
-    fetchProducts()
-}, [])
+    dispatch(listProducts())
+}, [dispatch])
  
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            }
         </div>
     )
 }
