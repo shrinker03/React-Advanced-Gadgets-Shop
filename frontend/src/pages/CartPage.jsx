6567abbb119765b49b30994500f37f62e3701210
shivam damre <shivamdamre913@gmail.com>
2021-07-05 12:14:17 +0530

    created: cartpage created, added: add item to the cart functionality added to the cart

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { addToCart } from '../actions/cartActions'
+import { useDispatch} from 'react-redux'
 
-const CartPage = () => {
+const CartPage = ({match, location, history}) => {
+    const productId = match.params.id
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    console.log(qty)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             Cart
