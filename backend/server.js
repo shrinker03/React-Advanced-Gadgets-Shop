42e6fe46772b9814bb698221094ab8ee93d145b5
shivam damre <shivamdamre913@gmail.com>
2021-07-07 11:00:17 +0530

    created: user Controller created with user route to make an api call to the users/login request

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors  from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {errorHandler, notFound} from './middleware/errorMiddeware.js'
 
 dotenv.config()
@@ -11,11 +12,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.send("API is running")
 })
 
 app.use('/products', productRoutes)
+app.use('/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
