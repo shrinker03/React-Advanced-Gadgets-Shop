67413900901ec07b89f95d05ac1fa68774fce0d8
shivam damre <shivamdamre913@gmail.com>
2021-07-07 09:59:47 +0530

    created: product routes controller created to refactor the route file

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc    FETCH ALL PRODUCTS
+// @route   GET /products
+// @access  PUBLIC 
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+// @desc    FETCH SINGLE PRODUCTS
+// @route   GET /products/:id
+// @access  PUBLIC 
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        res.json(product)
+    }else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export {getProducts, getProductById}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,8 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
-// @desc    FETCH ALL PRODUCTS
-// @route   GET /products
-// @access  PUBLIC 
-router.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
-
-// @desc    FETCH SINGLE PRODUCTS
-// @route   GET /products/:id
-// @access  PUBLIC 
-router.get("/:id", asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product) {
-        res.json(product)
-    }else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-    
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
