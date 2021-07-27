548ffc122ef159a92158339b340e422b1f6bd01f
shivam damre <shivamdamre913@gmail.com>
2021-07-27 11:29:05 +0530

    created: upload image functionality added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors  from 'colors'
@@ -8,6 +9,7 @@ import {errorHandler, notFound} from './middleware/errorMiddeware.js'
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -24,8 +26,12 @@ app.get("/", (req, res) => {
 app.use('/products', productRoutes)
 app.use('/users', userRoutes)
 app.use('/orders', orderRoutes)
+app.use('/upload', uploadRoutes)
 
 app.get('/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
