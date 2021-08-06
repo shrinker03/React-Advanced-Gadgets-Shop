a385d0e4ee4ef2b956774c39ccd7ab8b7d8823fc
shivam damre <shivamdamre913@gmail.com>
2021-08-06 09:41:57 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,11 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get("/", (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 } else {
-    app.get("/", (req, res) => {
+    app.get('/', (req, res) => {
         res.send("API is running")
     })
 }
