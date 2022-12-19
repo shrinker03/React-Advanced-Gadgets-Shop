7039b2e01b13165d75018884d999ee40108cc346
DESKTOP-65CV4VO\Acer <shivamdamre913@gmail.com>
2022-12-19 12:52:48 +0530

    removed some unnecessary code

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,6 @@ app.use(errorHandler)
 
 const  PORT = process.env.PORT || 5000
 
-connectDB().then(() => {
-    app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
-})
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+// connectDB().then(() => {
+// })
